9c2d7fd92b748e191a2a7134bc870df473a685ac
unknown <pablomonzon95@gmail.com>
2023-02-05 15:01:39 +0100

    cosita agregada

--- src/repositories/notes/editNoteById.js
+++ src/repositories/notes/editNoteById.js
@@ -1,12 +1,12 @@
 const getPool = require("../../database/getPool");
 
 const editNoteById = async (noteToUpdate) => {
-  const { id, title, note, image, categoryId } = noteToUpdate;
+  const { id, title, note, image, public, categoryId } = noteToUpdate;
   const pool = getPool();
 
   await pool.query(
-    "UPDATE notes SET title = ?, note = ?,image = ? , categoryId = ? WHERE id = ? ",
-    [title, note, image, categoryId, id]
+    "UPDATE notes SET title = ?, note = ?,image = ? , public = ?, categoryId = ? WHERE id = ? ",
+    [title, note, image, public, categoryId,  id]
   );
 };
 module.exports = editNoteById;
