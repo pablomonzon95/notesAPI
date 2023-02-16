d0884d8571b9b6d6e384cad45f970fdb9fdef96a
unknown <eefee1987@gmail.com>
2023-02-16 17:48:42 +0100

    cambiado el select de las categorias

--- src/repositories/categories/selectCategories.js
+++ src/repositories/categories/selectCategories.js
@@ -2,9 +2,7 @@ const getPool = require("../../database/getPool");
 
 const selectCategories = async () => {
   const pool = getPool();
-  const [categories] = await pool.query(
-    "SELECT name from categories"
-  );
+  const [categories] = await pool.query("SELECT * from categories");
 
   return categories;
 };
